5acf8166baa81cb5cd5b5d751bec2570c02f196b
Josh Hale <josh_8d@hotmail.co.uk>
2019-01-26 00:56:10 +0000

    Added snail eyes

---
+++ Snail/Assets/Scripts/CopyRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CopyRotation : MonoBehaviour
+{
+
+	[SerializeField]
+	private Transform m_controllerTransform = null;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var controllerRotation = m_controllerTransform.localEulerAngles;
+		var cameraRotation = transform.localEulerAngles;
+		cameraRotation.y = controllerRotation.y;
+
+		transform.localRotation = Quaternion.Euler(cameraRotation);
+	}
+}
