91a5bfe0b444bf1aedc4da7f8e23c6a1659e8a2c
Josh Hale <josh_8d@hotmail.co.uk>
2019-01-27 13:16:19 +0000

    Added Proximity and Death Warnings

--- Snail/Assets/Scripts/SnailAnimation.cs
+++ Snail/Assets/Scripts/SnailAnimation.cs
@@ -6,17 +6,36 @@ public class SnailAnimation : MonoBehaviour {
 
 	Animator[] anims;
 
+	bool dead = false;
+
 	// Use this for initialization
 	void Start () {
 		anims = GetComponentsInChildren<Animator>();
-		
+		dead = false;
+		SnailCollision.DeathEnter += Dead;
 	}
 
 	public void UpdateAnimSpeed(float speed)
 	{
+		if (dead)
+		{
+			return;
+		}
 		for (int i = 0; i < anims.Length; i++)
 		{
 			anims[i].SetFloat("speed", speed);
+		}
+	}
+
+	/// <summary>
+	/// Callback to draw gizmos only if the object is selected.
+	/// </summary>
+	public void Dead()
+	{
+		dead = true;
+		for (int i = 0; i < anims.Length; i++)
+		{
+			anims[i].SetFloat("speed", 0.0f);
 		}
 	}
 }
